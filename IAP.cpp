078153de39c6bf644e66ac10e39315d5415e3c72
uwePhillPhelps <philip3.phelps@uwe.ac.uk>
2023-12-15 14:50:56 +0000

    Add files via upload

---
+++ IAP.cpp
@@ -0,0 +1,15 @@
+// //// //// //// //// //// //// //// //// //// //// //// //// ////
+//  IAP.cpp
+//
+//  Originally created by Samuel Hunt on 16/07/2018.
+//  Edited by Phill Phelps on 15/12/2023.
+//
+
+#include "IAP.h"
+#include <iostream>
+
+void IAP::run()
+{
+    std::cout << "Hello moon!";
+    std::cout << "\n";
+}
---
+++ IAP.h
@@ -0,0 +1,38 @@
+// //// //// //// //// //// //// //// //// //// //// //// //// ////
+//
+//  IAP.h
+//
+//  Original created by Samuel Hunt on 16/07/2018.
+//  Updated  by Phill Phelps on 15/12/2023.
+//
+
+#pragma once
+
+#include "AserveClient.h"
+
+class IAP : public AserveClient  {
+public:  
+    //----------------------------------------------------------------------
+    // SHARED VARIABLES (IAP member variables)
+    // ... delete this line and add your own variable declarations below
+  
+    //----------------------------------------------------------------------
+    // FUNCTIONS (IAP class methods)
+    // ... delete this line and add your own function declarations below
+  
+    void run ();
+  
+    //---------------------------------------------------------------------
+    // CALLBACK FUNCTIONS
+    // ... delete this line and enable/disable callback functions below
+  
+    //void callbackNoteReceived  (int note, int velocity, int channel);
+    //void callbackModWheelMoved (int value);
+    //void callbackPitchbendWheelMoved (int value);
+    //void callbackCCValueChanged (int cc, int value);
+    //void callbackMIDIReceived (MIDI message);
+    //void callbackPixelGrid (int x, int y);
+
+private:
+
+};
